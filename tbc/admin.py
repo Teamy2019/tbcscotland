a50a820d5bd639f29d66de181b3a82cd8201414d
Stuart Rawlinson <s.rawlinson89@gmail.com>
2019-02-20 18:54:21 +0000

    Started working on the admin/models - may need to talk through views etc with everyone

--- tbc/admin.py
+++ tbc/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tbc.models import Category, Ad, Inbox, Profile
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Ad)
+admin.site.register(Inbox)
+admin.site.register(Profile)
--- tbc/models.py
+++ tbc/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Should this be from a pre-determined list? Like the shirt sizes
+# here: https://docs.djangoproject.com/en/2.1/topics/db/models/
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    views = models.IntegerField(default=0)
+
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='profile_images', blank=True)
+    skills = models.TextField()
+    education = models.TextField()
+    aboutme = models.TextField()
+    slug = models.SlugField(unique=True)
+    portfolio = models.ImageField(upload_to='portfolio_images', blank=True)
+    activities = models.ImageField(upload_to='activities_images', blank=True)
+    views = models.IntegerField(default=0)
+    reviews = models.TextField()
+    # location
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=128)
+    username = models.CharField(max_length=128)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    category = models.ForeignKey(Category)
+    profile = models.ForeignKey(Profile)
+    title = models.CharField(max_length=128)
+    # startdate
+    # enddate
+    description = models.TextField()
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='ad_images', blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Ad, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
+class Inbox(models.Model):
+    ad = models.ForeignKey(Ad)
+    messages = models.TextField()
+    sender = models.ForeignKey(Profile)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Inbox, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
